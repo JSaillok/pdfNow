cc0273de68236c87db8989a09485e99a07a50eb5
Ioannis <johndday07@gmail.com>
2023-09-07 17:41:22 +0300

    clean up

--- gui.py
+++ gui.py
@@ -2,8 +2,7 @@ import tkinter
 import customtkinter
 
 customtkinter.set_appearance_mode("Dark")
-# customtkinter.set_default_color_theme("dark-blue")
-customtkinter.set_default_color_theme("/home/jsaillok/Documents/pdfNow/theme.json")
+customtkinter.set_default_color_theme("theme.json")
 
 app = customtkinter.CTk()
 app.geometry("720x480")
@@ -25,22 +24,3 @@ button = customtkinter.CTkButton(master=app,
 button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
 app.mainloop()
-# layout = [
-#     [sg.Titlebar("Welcome to pdfNow")],
-
-#     [sg.Text("What you want to do today?")],
-#     [sg.Button("Option1")],
-#     [sg.Button("Option2")],
-#     [sg.Button("Option3")],
-#     [sg.Button("Option4")]
-# ]
-
-# window = sg.Window("pdfNow",layout, margins=(720,480))
-
-# while True:
-#     event, values= window.read()
-
-#     if event == "Option1" or event == sg.WIN_CLOSED:
-#         break
-
-# window.close()
